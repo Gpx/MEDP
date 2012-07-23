5d9d581699f8f0fd764cabb7cdc5f14c6af68128
Gpx <polvara@gmail.com>
2012-07-23 17:46:06 +0200

    Added call to robot's engines library

--- MEDP.cpp
+++ MEDP.cpp
@@ -2,6 +2,10 @@
 
 const char MEDP::PROTOCOL_DIVIDER_CHAR = '#';
 const char MEDP::COMMANDS_DIVIDER_CHAR = '&';
+const char MEDP::FORWARD_COMMAND_CHAR = 'F';
+const char MEDP::BACKWARD_COMMAND_CHAR = 'B';
+const char MEDP::LEFT_TURN_COMMAND_CHAR = 'L';
+const char MEDP::RIGHT_TURN_COMMAND_CHAR = 'R';
 
 void MEDP::setup() {
   Serial.begin(9600); // Used to debug the application
@@ -10,24 +14,71 @@ void MEDP::setup() {
 void MEDP::execute(String message) {
   message = removeProtocolIdentifier(message);
 
+  /**
+    This while loop splits the message and
+    extract every single instruction
+  */
   int start = 0;
   int end = 0;
   while (end != -1) {
     end = message.indexOf(MEDP::COMMANDS_DIVIDER_CHAR, ++start);
     String command;
-    if (end != -1) {
-      command = message.substring(start, end);
-    } else {
-      command = message.substring(start);
-    }
+    command = (end != -1) ? message.substring(start, end) : message.substring(start);
     start = end;
 
-    //TODO interpret command and execute action
-    Serial.println(command);
+    executeCommand(command);
   }
+}
+
+void MEDP::executeCommand(String command) {
+  char commandIdentifier = command[0];
+
+  if (commandIdentifier == MEDP::FORWARD_COMMAND_CHAR) {
+    executeForward(command);
+  } else if (commandIdentifier == MEDP::BACKWARD_COMMAND_CHAR) {
+    executeBackward(command);
+  } else if (commandIdentifier == MEDP::LEFT_TURN_COMMAND_CHAR) {
+    executeTurnLeft(command);
+  } else if (commandIdentifier == MEDP::RIGHT_TURN_COMMAND_CHAR) {
+    executeTurnRight(command);
+  }
+}
+
+void MEDP::executeForward(String command){
+  int movementSize = parseInt(command.substring(1));
+
+  PoombaEngines pe;
+  pe.moveForward(movementSize);
+}
+
+void MEDP::executeBackward(String command){
+  int movementSize = parseInt(command.substring(1));
+
+  PoombaEngines pe;
+  pe.moveBackward(movementSize);
+}
+
+void MEDP::executeTurnLeft(String command){
+  int rotationDegrees = parseInt(command.substring(1));
+
+  PoombaEngines pe;
+  pe.turnLeft(rotationDegrees);
+}
+
+void MEDP::executeTurnRight(String command){
+  int rotationDegrees = parseInt(command.substring(1));
+
+  PoombaEngines pe;
+  pe.turnRight(rotationDegrees);
 }
 
 String MEDP::removeProtocolIdentifier(String message) {
   int dividerPosition = message.indexOf(MEDP::PROTOCOL_DIVIDER_CHAR);
   return message.substring(dividerPosition);
+}
+
+int MEDP::parseInt(String value) {
+  char charValue[value.length()+1];
+  value.toCharArray(charValue, sizeof(charValue));
+  return atoi(charValue);
 }
--- MEDP.h
+++ MEDP.h
@@ -2,6 +2,7 @@
 #define MEDP_H
 
 #include "Arduino.h"
+#include "PoombaEngines.h"
 
 /**
 Class to parse a MEDP message
@@ -16,8 +17,20 @@ private:
   /** Protocol constants declarations */
   static const char PROTOCOL_DIVIDER_CHAR;
   static const char COMMANDS_DIVIDER_CHAR;
+  static const char FORWARD_COMMAND_CHAR;
+  static const char BACKWARD_COMMAND_CHAR;
+  static const char LEFT_TURN_COMMAND_CHAR;
+  static const char RIGHT_TURN_COMMAND_CHAR;
+  
+  void executeCommand(String command);   /** Parse anche execute the command */
+  void executeForward(String command);   /** Parse and execute F command */
+  void executeBackward(String command);  /** Parse and execute B command */
+  void executeTurnLeft(String command);  /** Parse and execute L command */
+  void executeTurnRight(String command); /** Parse and execute R command */
 
+  /** Utility functions */
   String removeProtocolIdentifier(String message); /** Return the message without protocol identifier */
+  int parseInt(String value);                      /** Parse a string into an int value */
 };
 
 #endif
